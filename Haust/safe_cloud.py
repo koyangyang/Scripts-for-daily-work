ddf0a8ad6a6b7743a50298b3202f1c72f84f1e2c
turbo1126 <66809557+turbo1126@users.noreply.github.com>
2021-11-19 16:38:43 +0800

    Create safe_cloud.py

---
+++ Haust/safe_cloud.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+import requests
+import time
+import json
+
+
+def safe():
+    url = "https://yqfkfw.haust.edu.cn/smart-boot/api/healthReport/saveHealthReport"
+    nowday = time.strftime("%Y-%m-%d %T", time.localtime())
+    #now
+    payload = json.dumps({
+        "address": "河南省洛阳市涧西区联盟路",
+        "age": "20",
+        "bodyTemperature": 36,
+        "createTime": "",
+        "latitude": "34.65922",
+        "longitude": "112.372713",
+        "isStayLocal": 1,
+        "phone": "填入手机号",
+        "needUpdate": 1
+    })
+    headers ={
+        'Content-Type': 'application/json',
+        'X-Id-Token': '填入token'
+    }
+    for h in headers:
+        response = requests.request("POST", url, headers=h, data=payload)
+        content=response.text[:-1]+',"time":'+nowday+"}"
+        #开启推送请取消下面一行注释
+        #push_wechat(content)
+
+
+def push_wechat(content):
+    #填入pushplus的token
+    token = ''
+    push_url = "http://www.pushplus.plus/send?token=" + token + "&title=报平安信息&content=" + content + "&template=json"
+    requests.get(url=push_url)
+
+
+def main_handler(event, context):    
+    safe()
